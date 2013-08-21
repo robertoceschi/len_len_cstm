bd78146889132f891d4addf6a9e6ddceef3cbf66
roberto_arbeit <robertoceschi>
2013-08-21 10:58:54 +0200

    masonry_basic_version

--- js/main.js
+++ js/main.js
@@ -18,5 +18,18 @@ jQuery(document).ready(function($) {
         showSidebar();
     });
     jQuery(".headline").fitText();
+
+    // Initialize Masonry
+    $('#content').masonry({
+        columnWidth: 320,
+        itemSelector: '.item',
+        isFitWidth: true,
+        isAnimated: !Modernizr.csstransitions
+    }).imagesLoaded(function() {
+            $(this).masonry('reload');
+        });
 });
+
+
+
 
