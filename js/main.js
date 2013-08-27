d6c0c8b0be956ee5080b623e40abe5301b9f5e55
roberto_arbeit <robertoceschi>
2013-08-27 14:28:41 +0200

    off canvas banner working version

--- js/main.js
+++ js/main.js
@@ -1,34 +1,23 @@
 // Make the showSidebar function into a variable so it can be easily called
 var showSidebar = function () {
     var $target = $('body').toggleClass("active");
+    //Anpassungen für OFF-CanvasBanner
     if ($target.hasClass('active')) {
-        // $('.outer-header').css('position', 'absolute');
-        // $('.outer-header').css('background', 'white');
-        //$('header[role="banner"]').css('visibility', 'hidden')
-        //$('.main-content').prepend("<span class='bannerOn'><a href='#banner_fix'><span aria-hidden='true' data-icon='&#xe000;' class='down-arrow '></span></span></a>")
-        // $('header[role="banner"]').add("<p id='new'>new paragraph</p>").css('color', 'red');
-        //alert('sali');
-        $('header[role="banner"] .sidebar-button').css('left', '89%')
-
+        $('.main-link-headline').css('display', 'none');
     }
     if (!$target.hasClass('active')) {
-          //$('.outer-header').css('position', 'fixed');
-        //$('.outer-header').css('background', 'rgba(255, 255, 255, 0.7)');
-        //alert('sali_du');
-        $('header[role="banner"] .sidebar-button').css('left', '2%')
+        $('.main-link-headline').css('display', 'block');
     }
 };
-
 
 // add/remove classes everytime the window resize event fires
 jQuery(window).resize(function () {
     var off_canvas_nav_display = $('.off-canvas-navigation').css('display');
     if (off_canvas_nav_display === 'block') {
-        //$("body").removeClass("active");
+        $("body").removeClass("active");
 
     }
 });
-
 
 jQuery(document).ready(function ($) {
     // Toggle for sidebar
@@ -37,15 +26,17 @@ jQuery(document).ready(function ($) {
         showSidebar();
     });
 
-    jQuery("#sidebar_button").click(function(){
+    jQuery("#sidebar_button").click(function () {
 
         $('#banner_toggle').toggleClass("banner_new");
     });
+
     jQuery(".headline").fitText();
 
+    // Fittext
     jQuery(".headline").fitText(1.2, { minFontSize: '24px', maxFontSize: '120px' });
-    jQuery("#main_image p").fitText(3,{ minFontSize: '18px', maxFontSize: '90px' });
-    jQuery("#main_image p").fitText(3,{ minFontSize: '18px', maxFontSize: '90px' });
+    jQuery("#main_image p").fitText(3, { minFontSize: '18px', maxFontSize: '90px' });
+    jQuery("#main_image p").fitText(3, { minFontSize: '18px', maxFontSize: '90px' });
 
     // Initialize Masonry
     $('#content').masonry({
